53ed46d2853737e642414da6a5b52b37b0b7c666
Isha Hasan <65569339+ishahasan69@users.noreply.github.com>
2026-02-01 23:47:39 +0500

    Updates

--- component/Benifits/Benifits.tsx
+++ component/Benifits/Benifits.tsx
@@ -7,98 +7,124 @@ import { Space } from "../Space";
 import { Text } from "../Text";
 import classes from "./Benifits.module.scss";
 
-
 const UnmemoBenifits = () => {
-  return(
+  return (
     <div className="container">
       <div className="row">
         <div className="col-lg-2"></div>
         <div className="col-lg-8">
           <div className={classes.Benefits}>
-          <Text tag={htmlTag.h4}>Benefits of choosing Us</Text>
-          <div className={classes.provideTechnical}>We provide technical support on a ‍24x7 basis for our customers.</div>
+            <Text tag={htmlTag.h4}>Benefits of choosing Us</Text>
+            <div className={classes.provideTechnical}>
+              We provide technical support on a ‍24x7 basis for our customers.
+            </div>
           </div>
         </div>
         <div className="col-lg-2"></div>
       </div>
 
-      <Space size={componentSizes.large}/>
-      <Space size={componentSizes.large}/>
-      <Space size={componentSizes.large}/>
-      <Space size={componentSizes.large}/>
-      <Space size={componentSizes.small}/>
+      <Space size={componentSizes.large} />
+      <Space size={componentSizes.large} />
+      <Space size={componentSizes.large} />
+      <Space size={componentSizes.large} />
+      <Space size={componentSizes.small} />
 
-      <div className="row">
-        <div className="col-lg-2"></div>
-        <div className="col-lg-8">
-          <div className="row">
-            <div className="col-lg-6">
-              <div className={classes.upperleft}>
-                <Image
-                src="/Logo.png"
-                alt="Galaxy Telecom Logo"
-                width={80}
-                height={80}
-              />
-                <Space size={componentSizes.small}/>
-                <Space size={componentSizes.small}/>
-                <Space size={componentSizes.small}/>
-                <div className={classes.heading}>24 Hour Support</div>
-                <Space size={componentSizes.small}/>
-                <Text size={componentSizes.regular}>We provide a complete turnkey solution to our clients from network design, hardware selection, installation, commissioning & training. A complete end-to-end resolution is provided to run a seamless network.</Text>
+      <div className={classes.benefitsContainer}>
+        <div className="row">
+          <div className="col-lg-2"></div>
+          <div className="col-lg-8">
+            <div className="row">
+              <div className="col-lg-6">
+                <div className={classes.upperleft}>
+                  <Image
+                    src="/Logo.png"
+                    alt="Galaxy Telecom Logo"
+                    width={80}
+                    height={80}
+                  />
+                  <Space size={componentSizes.small} />
+                  <Space size={componentSizes.small} />
+                  <Space size={componentSizes.small} />
+                  <div className={classes.heading}>24 Hour Support</div>
+                  <Space size={componentSizes.small} />
+                  <Text size={componentSizes.regular}>
+                    We provide a complete turnkey solution to our clients from
+                    network design, hardware selection, installation,
+                    commissioning & training. A complete end-to-end resolution
+                    is provided to run a seamless network.
+                  </Text>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className={classes.upperleft}>
+                  <Image
+                    src="/Logo.png"
+                    alt="Galaxy Telecom Logo"
+                    width={80}
+                    height={80}
+                  />
+                  <Space size={componentSizes.small} />
+                  <Space size={componentSizes.small} />
+                  <Space size={componentSizes.small} />
+                  <div className={classes.heading}>24 Hour Support</div>
+                  <Space size={componentSizes.small} />
+                  <Text size={componentSizes.regular}>
+                    We provide a complete turnkey solution to our clients from
+                    network design, hardware selection, installation,
+                    commissioning & training. A complete end-to-end resolution
+                    is provided to run a seamless network.
+                  </Text>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className={classes.upperleft}>
+                  <Image
+                    src="/Logo.png"
+                    alt="Galaxy Telecom Logo"
+                    width={80}
+                    height={80}
+                  />
+                  <Space size={componentSizes.small} />
+                  <Space size={componentSizes.small} />
+                  <Space size={componentSizes.small} />
+                  <div className={classes.heading}>24 Hour Support</div>
+                  <Space size={componentSizes.small} />
+                  <Text size={componentSizes.regular}>
+                    We provide a complete turnkey solution to our clients from
+                    network design, hardware selection, installation,
+                    commissioning & training. A complete end-to-end resolution
+                    is provided to run a seamless network.
+                  </Text>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className={classes.upperleft}>
+                  <Image
+                    src="/Logo.png"
+                    alt="Galaxy Telecom Logo"
+                    width={80}
+                    height={80}
+                  />
+                  <Space size={componentSizes.small} />
+                  <Space size={componentSizes.small} />
+                  <Space size={componentSizes.small} />
+                  <div className={classes.heading}>24 Hour Support</div>
+                  <Space size={componentSizes.small} />
+                  <Text size={componentSizes.regular}>
+                    We provide a complete turnkey solution to our clients from
+                    network design, hardware selection, installation,
+                    commissioning & training. A complete end-to-end resolution
+                    is provided to run a seamless network.
+                  </Text>
+                </div>
               </div>
             </div>
-            <div className="col-lg-6"><div className={classes.upperleft}>
-                <Image
-                src="/Logo.png"
-                alt="Galaxy Telecom Logo"
-                width={80}
-                height={80}
-              />
-                <Space size={componentSizes.small}/>
-                <Space size={componentSizes.small}/>
-                <Space size={componentSizes.small}/>
-                <div className={classes.heading}>24 Hour Support</div>
-                <Space size={componentSizes.small}/>
-                <Text size={componentSizes.regular}>We provide a complete turnkey solution to our clients from network design, hardware selection, installation, commissioning & training. A complete end-to-end resolution is provided to run a seamless network.</Text>
-              </div></div>
-            <div className="col-lg-6">
-              <div className={classes.upperleft}>
-                <Image
-                src="/Logo.png"
-                alt="Galaxy Telecom Logo"
-                width={80}
-                height={80}
-              />
-                <Space size={componentSizes.small}/>
-                <Space size={componentSizes.small}/>
-                <Space size={componentSizes.small}/>
-                <div className={classes.heading}>24 Hour Support</div>
-                <Space size={componentSizes.small}/>
-                <Text size={componentSizes.regular}>We provide a complete turnkey solution to our clients from network design, hardware selection, installation, commissioning & training. A complete end-to-end resolution is provided to run a seamless network.</Text>
-              </div></div>
-            <div className="col-lg-6"><div className={classes.upperleft}>
-                <Image
-                src="/Logo.png"
-                alt="Galaxy Telecom Logo"
-                width={80}
-                height={80}
-              />
-                <Space size={componentSizes.small}/>
-                <Space size={componentSizes.small}/>
-                <Space size={componentSizes.small}/>
-                <div className={classes.heading}>24 Hour Support</div>
-                <Space size={componentSizes.small}/>
-                <Text size={componentSizes.regular}>We provide a complete turnkey solution to our clients from network design, hardware selection, installation, commissioning & training. A complete end-to-end resolution is provided to run a seamless network.</Text>
-              </div>
-              </div>
           </div>
+          <div className="col-lg-2"></div>
         </div>
-        <div className="col-lg-2"></div>
       </div>
     </div>
-  )
-  
+  );
 };
 
 export const Benifits = memo(UnmemoBenifits);
