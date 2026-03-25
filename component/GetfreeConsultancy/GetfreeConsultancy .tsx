6220dafce3ffe0aa84e57056599006cc8882535f
ishahasan <dabeerhasan15@gmail.com>
2026-03-25 19:50:06 +0500

    updates

--- component/GetfreeConsultancy/GetfreeConsultancy .tsx
+++ component/GetfreeConsultancy/GetfreeConsultancy .tsx
@@ -1,46 +1,42 @@
 "use client";
 
 import { useContactModal } from "@/context/ContactModalContext";
-import { componentSizes } from "@/types";
 import Image from "next/image";
 import { memo } from "react";
-import { Space } from "../Space";
 import classes from "./GetfreeConsultancy.module.scss";
 
-
-
-
-const UnmemoGetfreeConsultancy  =  () => {
+const UnmemoGetfreeConsultancy = () => {
   const { openContactModal } = useContactModal();
 
-  return(
+  return (
     <div className="container">
-        <div className="row">
-              <div className={classes.freeConsultancy}>
-              <div className={classes.content}>
-                <div className={classes.heading}>Get free Consultancy <br/> with our experts!</div>
-                <Space size={componentSizes.large}/>
-                <Space size={componentSizes.medium}/>
-                
-                <button onClick={openContactModal} className={classes.tag}>Learn More</button>
-              </div>
+      <div className="row">
+        <div className={classes.freeConsultancy}>
+          <div className={classes.content}>
+            <div className={classes.heading}>
+              Get free Consultancy <br /> with our experts!
+            </div>
 
-              <div className={classes.image}>
-                <div className={classes.imageWrapper}>
-                  <Image
-                    src="/consultants.png"
-                    alt="Galaxy Telecom Logo"
-                    width={279}
-                    height={266}
-                    className={classes.img}
-                  />
-                </div>
-              </div>
-              </div>
+            <button onClick={openContactModal} className={classes.tag}>
+              Learn More
+            </button>
+          </div>
+
+          <div className={classes.image}>
+            <div className={classes.imageWrapper}>
+              <Image
+                src="/consultants.png"
+                alt="Galaxy Telecom Logo"
+                width={279}
+                height={266}
+                className={classes.img}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-  
+  );
 };
 
-export const GetfreeConsultancy  = memo(UnmemoGetfreeConsultancy );
+export const GetfreeConsultancy = memo(UnmemoGetfreeConsultancy);
